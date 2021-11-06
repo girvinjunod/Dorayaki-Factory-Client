8d1525c23170516fe1695f9dc4bd1e2e806354aa
girvinjunod <girvinjunod@gmail.com>
2021-11-07 03:31:06 +0700

    fix init nextjs with ts and tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="description" content="Dorayaki factory" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <div className="font-bold rounded text-center">
+      Dashboard
+      </div>
+      </div>
+  )
+}
+
+export default Home
